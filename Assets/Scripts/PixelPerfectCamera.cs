2260163f08e03d6452a15f43695306440609777b
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-20 18:10:17 -0600

    write script to auto fit screen

---
+++ Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	public static float pixelsToUnits = 1f;
+	public static float scale = 1f;
+
+	public Vector2 nativeResolution = new Vector2(240, 160);
+
+	void Awake() {
+		var camera = GetComponent<Camera>();
+
+		if (camera.orthographic) {
+			scale = Screen.height / nativeResolution.y;
+			pixelsToUnits *= scale;
+			camera.orthographicSize = (Screen.height / 2f) / pixelsToUnits;
+		}
+	}
+}
