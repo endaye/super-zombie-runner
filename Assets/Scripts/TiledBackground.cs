417349569823d4387c82df741285ae679405f61a
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-23 20:56:20 -0600

    Emulate parallax scrolling

--- Assets/Scripts/TiledBackground.cs
+++ Assets/Scripts/TiledBackground.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class TiledBackground : MonoBehaviour {
 
 	public int textureSize = 32;
+    public bool scaleHorizontally = true;
+    public bool scaleVertically = true;
 
 	// Use this for initialization
 	void Start () {
-		var newWidth = Mathf.Ceil (Screen.width / (textureSize * PixelPerfectCamera.scale));
-		var newHeight = Mathf.Ceil (Screen.height / (textureSize * PixelPerfectCamera.scale));
+        
+		var newWidth = !scaleHorizontally ? 1 : Mathf.Ceil (Screen.width / (textureSize * PixelPerfectCamera.scale));
+		var newHeight = !scaleVertically ? 1 : Mathf.Ceil (Screen.height / (textureSize * PixelPerfectCamera.scale));
 
 		transform.localScale = new Vector3 (newWidth * textureSize, newHeight * textureSize, 1);
 
