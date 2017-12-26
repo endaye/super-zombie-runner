88c753cb0d23cce8d9a0f0ae9863b9ff24fb7eac
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-25 23:31:25 -0600

    Destroy obstacles offscreen

---
+++ Assets/Scripts/DestroyOffscreen.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOffscreen : MonoBehaviour
+{
+
+    public float offset = 16f;
+
+    private bool offscreen;
+    private float offscreenX = 0f;
+    private Rigidbody2D body2d;
+
+    private void Awake()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        offscreenX = (Screen.width / PixelPerfectCamera.pixelsToUnits) / 2f + offset;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var posX = transform.position.x;
+        var dirX = body2d.velocity.x;
+
+        if (Mathf.Abs(posX) > offscreenX)
+        {
+            if (dirX < 0 && posX < -offscreenX)
+            {
+                offscreen = true;
+            }
+            else if (dirX > 0 && posX > offscreenX)
+            {
+                offscreen = true;
+            }
+        }
+        else
+        {
+            offscreen = false;
+        }
+
+        if (offscreen)
+        {
+            OnOutOfBounds();
+        }
+    }
+
+    public void OnOutOfBounds()
+    {
+        offscreen = false;
+        Destroy(gameObject);
+    }
+}
