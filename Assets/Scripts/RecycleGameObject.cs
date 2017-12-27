94f8edacfe64e125350280eca311aa449c7c7fea
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-26 23:46:17 -0600

    Implement recycle in any script

--- Assets/Scripts/RecycleGameObject.cs
+++ Assets/Scripts/RecycleGameObject.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public interface IRecycle
+{
+	void Restart ();
+	void Shutdown ();
+}
+
 public class RecycleGameObject : MonoBehaviour
 {
+
+	private List<IRecycle> recycleComponents;
+
+	void Awake ()
+	{
+		var components = GetComponents<MonoBehaviour> ();
+		recycleComponents = new List<IRecycle> ();
+		foreach (var component in components) {
+			if (component is IRecycle) {
+				recycleComponents.Add (component as IRecycle); 
+			}
+		}
+
+		Debug.Log (name + " Found " + recycleComponents.Count + " Components");
+	}
 
 	public void Restart ()
 	{
 		gameObject.SetActive (true);
+
+		foreach (var component in recycleComponents) {
+			component.Restart ();
+		}
 	}
 
 	public void Shutdown ()
 	{
 		gameObject.SetActive (false);
+
+		foreach (var component in recycleComponents) {
+			component.Shutdown ();
+		}
 	}
 
 }
