23c8f71c7eee48d671524051e0c30911eaecf38c
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-27 17:58:18 -0600

    Detect when the player is standing

---
+++ Assets/Scripts/InputState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputState : MonoBehaviour {
+
+	public bool actionButton;
+	public float absVelX = 0f;
+	public float absVelY = 0f;
+	public bool standing;
+	public float standingThreshold = 1;
+
+	private Rigidbody2D body2d;
+
+	void Awake () {
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		actionButton = Input.anyKeyDown;
+	}
+
+	void FixedUpdate() {
+		absVelX = System.Math.Abs (body2d.velocity.x);
+		absVelY = System.Math.Abs (body2d.velocity.y);
+
+		standing = absVelY <= standingThreshold;
+	}
+}
