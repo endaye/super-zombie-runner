b62603a6d627d7c9ea8e8aebbfb843b3c3b04879
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-26 23:26:05 -0600

    Wire up the object pool

--- Assets/Scripts/GameObjectUtil.cs
+++ Assets/Scripts/GameObjectUtil.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameObjectUtil
 {
-	
+	private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool> ();
+
 	public static GameObject Instantiate (GameObject prefab, Vector3 pos)
 	{
 		GameObject instance = null;
 
-		instance = GameObject.Instantiate (prefab);
-		instance.transform.position = pos;
+		var recycleScript = prefab.GetComponent<RecycleGameObject> ();
+		if (recycleScript != null) {
+			var pool = GetObjectPool (recycleScript);
+			instance = pool.NextObject (pos).gameObject;
+		} else {
+			instance = GameObject.Instantiate (prefab);
+			instance.transform.position = pos;
+		}
 
 		return null;
 	}
@@ -24,5 +32,21 @@ public class GameObjectUtil
 		} else {
 			GameObject.Destroy (gameObject);
 		}
+	}
+
+	private static ObjectPool GetObjectPool (RecycleGameObject reference)
+	{
+		ObjectPool pool = null;
+
+		if (pools.ContainsKey (reference)) {
+			pool = pools [reference];
+		} else {
+			var poolContainer = new GameObject (reference.gameObject.name + "ObjectPool");
+			pool = poolContainer.AddComponent<ObjectPool> ();
+			pool.prefab = reference;
+			pools.Add (reference, pool);
+		}
+
+		return pool;
 	}
 }
