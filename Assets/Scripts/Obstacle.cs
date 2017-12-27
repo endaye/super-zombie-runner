08ea8fa64db7c0dcdca143c50a78c29b88b3668c
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-26 23:57:26 -0600

    Clean up the obstacles

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IRecycle {
+
+	public Sprite[] sprites;
+
+	public void Restart() {
+		var renderer = GetComponent<SpriteRenderer> ();
+		renderer.sprite = sprites [Random.Range (0, sprites.Length)];
+	}
+
+	public void Shutdown() {}
+}
