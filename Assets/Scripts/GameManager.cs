8c6a59c96ff2e19e5bc66b4cd5aaa671ff0fe627
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-28 09:43:25 -0600

    Start the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject floor;
+	public Spawner spawner;
+
+	void Awake() {
+		floor = GameObject.Find ("Foreground");
+		spawner = GameObject.Find ("Spawner").GetComponent<Spawner> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		var floorHeight = floor.transform.localScale.y;
+		var pos = floor.transform.position;
+		pos.x = 0;
+		pos.y = -(Screen.height / PixelPerfectCamera.pixelsToUnits / 2) + (floorHeight / 2);
+		floor.transform.position = pos;
+
+		spawner.active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
