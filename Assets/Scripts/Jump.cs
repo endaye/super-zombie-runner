3f6d1a427b9e44b924d6e46896d1be952d470a39
Yuancheng Zhang <old.far.hot@gmail.com>
2017-12-27 18:08:34 -0600

    Make the player jump

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+
+	public float jumpSpeed = 240f;
+	public float forwardSpeed = 20f;
+
+	private Rigidbody2D body2d;
+	private InputState inputState;
+
+	void Awake() {
+		body2d = GetComponent<Rigidbody2D> ();
+		inputState = GetComponent<InputState> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (inputState.standing) {
+			if (inputState.actionButton) {
+				body2d.velocity = new Vector2 (transform.position.x < 0 ? forwardSpeed : 0, jumpSpeed);
+			}
+		}
+	}
+}
